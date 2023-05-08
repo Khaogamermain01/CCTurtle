22e0737f75264b549b1a976affae392fe58cdc84
Khaogamermain01 <62739017+Khaogamermain01@users.noreply.github.com>
2023-05-08 20:46:45 +0700

    a

--- FarmingBot.lua
+++ FarmingBot.lua
@@ -1,30 +1,33 @@
 local success, data = turtle.inspectDown()
 
-if not data.name == "minecraft:wheat" then
+if not success or data.name ~= "minecraft:wheat" then
     print("Place Turtle on Wheat!")
     return
 end
 
 function findItemSlot(itemName)
     for slot = 1, 16 do 
-        if turtle.getItemDetail(slot) == itemName then 
+        local item = turtle.getItemDetail(slot)
+        if item and item.name == itemName then 
             return slot 
         end
     end
     return nil
 end
 
-
-turned = true
 while true do
-    local success, data = turtle.inspectDown()
-    if success then
-        if data.name == "minecraft:wheat" then
+    success, data = turtle.inspectDown()
+    if success and data.name == "minecraft:wheat" then
+        if data.state.age == 7 then
             turtle.digDown()
             turtle.placeDown()
             turtle.forward()
+        else
+            os.sleep(300)  -- wait 5 minutes before checking again
         end
     else
         turtle.back()
+        turtle.turnLeft()
+        turtle.turnLeft()
     end
 end
