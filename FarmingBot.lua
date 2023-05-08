f25fe01c559fafd7ea082972e23a25263d0e1bbc
Khaogamermain01 <62739017+Khaogamermain01@users.noreply.github.com>
2023-05-08 21:43:06 +0700

    a

--- FarmingBot.lua
+++ FarmingBot.lua
@@ -1,33 +1,58 @@
-local success, data = turtle.inspectDown()
+-- Set the starting position of the turtle
+local startX, startY, startZ = gps.locate()
 
-if not success or data.name ~= "minecraft:wheat" then
-    print("Place Turtle on Wheat!")
-    return
+-- Define the dimensions of the wheat farm
+local farmWidth = 9
+local farmLength = 9
+
+-- Define the coordinates of the chest above the starting position
+local chestX, chestY, chestZ = startX, startY + 1, startZ
+
+-- Define the slot number of the chest item
+local chestSlot = 1
+
+-- Set the turtle to face forward
+turtle.forward()
+
+-- Loop through each row of the farm
+for row = 1, farmLength do
+
+  -- Harvest the wheat in the current row
+  for col = 1, farmWidth do
+    turtle.digDown()
+    turtle.forward()
+  end
+
+  -- Return to the start of the row
+  if row % 2 == 0 then
+    turtle.turnRight()
+    turtle.forward()
+    turtle.turnRight()
+  else
+    turtle.turnLeft()
+    turtle.forward()
+    turtle.turnLeft()
+  end
+
 end
 
-function findItemSlot(itemName)
-    for slot = 1, 16 do 
-        local item = turtle.getItemDetail(slot)
-        if item and item.name == itemName then 
-            return slot 
-        end
-    end
-    return nil
+-- Return the turtle to its starting position
+turtle.turnRight()
+turtle.turnRight()
+for i = 1, farmLength do
+  turtle.forward()
+end
+turtle.turnRight()
+turtle.turnRight()
+for i = 1, farmWidth do
+  turtle.forward()
 end
 
-while true do
-    success, data = turtle.inspectDown()
-    if success and data.name == "minecraft:wheat" then
-        if data.state.age == 7 then
-            turtle.digDown()
-            turtle.placeDown()
-            turtle.forward()
-        else
-            os.sleep(300)  -- wait 5 minutes before checking again
-        end
-    else
-        turtle.back()
-        turtle.turnLeft()
-        turtle.turnLeft()
-    end
-end
+-- Deposit the harvested wheat into the chest
+turtle.select(chestSlot)
+turtle.placeUp()
+
+-- Return to the starting position
+turtle.back()
+turtle.turnRight()
+turtle.turnRight()
